bbaa96fcb97f2fc0ac7dc61855d2796673a20f11
dajolol <dajolol88@gmail.com>
2023-10-11 17:42:36 +0200

    added points counter to method

--- Program.cs
+++ Program.cs
@@ -7,18 +7,16 @@ class Program
         int poäng = 0;
         Console.WriteLine("Välkommen till Quizet!");
 
-        AskQuestions("Fråga 1: Vilket är huvudstaden i Frankrike?", "paris"); // fråga 1
-
-        AskQuestions("Fråga 2: Vilken brukar kallas för den röda planeten?", "mars"); // fråga 2
-
-        AskQuestions("Fråga 3: Vilken hundras är Lassie?", "collie"); // fråga 3
+        AskQuestions("Fråga 1: Vilket är huvudstaden i Frankrike?", "paris", ref poäng); // fråga 1
+        AskQuestions("Fråga 2: Vilken brukar kallas för den röda planeten?", "mars", ref poäng); // fråga 2
+        AskQuestions("Fråga 3: Vilken hundras är Lassie?", "collie", ref poäng); // fråga 3
 
         //Console.WriteLine("Fråga 4: Skriv in en siffra som delat med 10 ska bli 5", ); // fråga 4
 
         Console.WriteLine($"Nu är quizet klart. Du fick {poäng} poäng.");
     }
 
-    static void AskQuestions(string fråga, string svaretPåFrågan)
+    static int AskQuestions(string fråga, string svaretPåFrågan, ref int currentPoints)
     {
         Console.WriteLine(fråga);
 
@@ -27,10 +25,13 @@ class Program
         if (svar.ToLower() == svaretPåFrågan)
         {
             Console.WriteLine("Rätt!");
+            currentPoints++;
+            return currentPoints;
         }
         else
         {
             Console.WriteLine("Fel!");
+            return currentPoints;
         }
     }
 }
